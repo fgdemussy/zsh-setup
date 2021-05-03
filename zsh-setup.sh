c202eafaf30a2825591ce71f22720e0530ae4a04
françois <fgdemussy@gmail.com>
2021-05-03 09:10:37 -0400

    feat: source .zshrc at the end

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,3 +3,4 @@ echo "syntax on" >> ~/.vimrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 cp ./.zshrc ~/.zshrc
+source ~/.zshrc
