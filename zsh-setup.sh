d16e4e1c0d7c25afedee894550182c798db45c5f
françois <fgdemussy@gmail.com>
2021-05-04 01:35:38 -0400

    feat: unattended omz install

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -23,7 +23,7 @@ vim_syntax_on() {
 }
 
 install_zsh() {
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 }
 
 install_plugins() {
